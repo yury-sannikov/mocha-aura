7924ada2e132d05a6fde9bc744715ebc4ceda768
Yuriy Sannikov <ysannikov@fonteva.com>
2017-06-19 11:55:50 -0400

    fix lib name case

--- aura.js
+++ aura.js
@@ -1,7 +1,7 @@
 const { eventFactory } = require('./lib/eventFactory')
 const { componentFactory, useComponentAdapters } = require('./lib/componentFactory')
 const { auraFactory } = require('./lib/auraFactory')
-const { AuraUtil } = require('./lib/AuraUtil')
+const { AuraUtil } = require('./lib/auraUtil')
 const { apexCallFactory, apexSuccessResult, apexErrorResult } = require('./lib/apexCallFactory')
 
 module.exports = {
